cb9fecdfe43cd2ff2ed2081e86f90e744c32cc2b
scarlisle <s.m.carlisle@gmail.com>
2019-03-21 16:11:43 -0500

    +1

--- src/main/java/org/mavriksc/superpermuter/SuperPermuter.java
+++ src/main/java/org/mavriksc/superpermuter/SuperPermuter.java
@@ -1,22 +1,29 @@
 package org.mavriksc.superpermuter;
 
+import org.mavriksc.superpermuter.type.XY;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 
 public class SuperPermuter {
 
-    private static List<String> source = asList("A", "B", "C", "D", "E","F");
+    private static List<String> source = asList("A", "B", "C", "D", "E");
     private static int N = source.size();
+    static private Map<String, XY> lookup = new HashMap<>();
 
 
 
     public static void main(String[] args){
-        
+        String[][] permutations = permutationsViaRotation(source);
+
+        outputPerm2DArray(permutations);
+
     }
 
     private static List<String> returnPermutations(List<String> source, int choose, int len) {
@@ -46,17 +53,24 @@ public class SuperPermuter {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < row1.size(); j++) {
-                perms[i][j] = i == 0 ? row1.get(j) : rotateString(perms[i - 1][j]);
+                perms[i][j] = i == 0 ? row1.get(j) : rotateStringRight(perms[i - 1][j]);
+                lookup.put(perms[i][j],new XY(i,j));
             }
         }
         return perms;
     }
 
-    private static String rotateString(String s) {
+    private static String rotateStringLeft(String s) {
         if (s == null || s.length() == 1) return s;
         else {
-            StringBuilder sb = new StringBuilder();
-            return sb.append(s.substring(1)).append(s.substring(0, 1)).toString();
+            return s.substring(1) + s.substring(0, 1);
+        }
+    }
+
+    private static String rotateStringRight(String s){
+        if (s == null || s.length() == 1) return s;
+        else {
+            return s.substring(s.length()-1) + s.substring(0, s.length()-1);
         }
     }
 
@@ -74,5 +88,15 @@ public class SuperPermuter {
         return isSuperPermutation(Arrays.asList(symbols.split("")),guess);
     }
 
-
+    private static void outputPerm2DArray(String[][] permutations) {
+        StringBuilder sb = new StringBuilder();
+        for (String[] permutation : permutations) {
+            for (String s : permutation) {
+                sb.append(s).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+    
 }
---
+++ src/main/java/org/mavriksc/superpermuter/type/XY.java
@@ -0,0 +1,11 @@
+package org.mavriksc.superpermuter.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class XY {
+    private int x;
+    private int y;
+}
